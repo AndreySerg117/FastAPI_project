fd83fce264e66167b37b969e7ce77682c3ae10fd
Andrey <Andrey827541@gmail.com>
2025-06-14 21:05:19 +0300

    rabbit last change

--- backend_api/app/applications/users/router.py
+++ backend_api/app/applications/users/router.py
@@ -1,6 +1,6 @@
 import uuid
 
-from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi import APIRouter, Depends, status, HTTPException, Request, BackgroundTasks
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from applications.users.crud import create_user_in_db, get_user_by_email, activate_user_account
@@ -11,7 +11,12 @@ router_users = APIRouter()
 
 
 @router_users.post("/create", status_code=status.HTTP_201_CREATED)
-async def create_user(new_user: RegisterUserFields, session: AsyncSession = Depends(get_async_session)) -> BaseUserInfo:
+async def create_user(
+        request: Request,
+        new_user: RegisterUserFields,
+        background_task: BackgroundTasks,
+        session: AsyncSession = Depends(get_async_session),
+) -> BaseUserInfo:
     user = await get_user_by_email(new_user.email, session)
     if user:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='Already exists')
---
+++ backend_api/app/services/rabbit/constants.py
@@ -0,0 +1,4 @@
+from enum import StrEnum
+
+class SupportedQueues(StrEnum):
+    USER_REGISTRATION = 'user_registration'
