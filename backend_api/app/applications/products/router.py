dc9fabf07feddedd8d44e530c5bc7fbf7bce6d11
Andrey <Andrey827541@gmail.com>
2025-06-18 19:31:42 +0300

    products add images

--- backend_api/app/applications/products/router.py
+++ backend_api/app/applications/products/router.py
@@ -1,12 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Body, UploadFile
 
 products_router = APIRouter()
 
 
 @products_router.post('/')
-async def create_product():
+async def create_product(
+    main_image: UploadFile,
+    images: list[UploadFile],
+    title: str = Body(max_length=100),
+    description: str = Body(max_length=1000),
+    price: float = Body(gt=1),
+):
     return
-
 
 @products_router.get('/{pk}')
 async def get_product(pk: int):
