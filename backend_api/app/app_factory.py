11fa9e47493cbba351e76b32204cc27a6e83d045
Andrey <Andrey827541@gmail.com>
2025-06-18 18:46:06 +0300

    create products and fix error

--- backend_api/app/app_factory.py
+++ backend_api/app/app_factory.py
@@ -3,13 +3,12 @@ from fastapi import FastAPI
 from applications.auth.router import router_auth
 from applications.users.router import router_users
 from settings import settings
+from applications.products.router import products_router
 
 import sentry_sdk
 
 sentry_sdk.init(
     dsn="https://e9841f2531bd8b7537b6338c9643b4e9@o4509457148346368.ingest.de.sentry.io/4509478950010960",
-    # Add data like request headers and IP for users,
-    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
     send_default_pii=True,
 )
 
@@ -17,5 +16,6 @@ def get_application() -> FastAPI:
     app = FastAPI(root_path="/api", root_path_in_servers=True, debug=settings.DEBUG)
     app.include_router(router_users, prefix="/users", tags=["Users"])
     app.include_router(router_auth, prefix="/auth", tags=["Auth"])
+    app.include_router(products_router, prefix="/products", tags=['Products'])
 
     return app
